d18505b02216cbf19fbf3e5dc8e689a9d67ec36a
Raja kumar <rajaraja360parth@gmail.com>
2024-06-29 15:37:23 +0530

    passing and handling the props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -19,8 +19,8 @@ function App() {
     <>
       <h1 className='bg-green-400 text-black p-4 rounded mb-4'>Tailwind Test</h1>
 
-      <Card channel="chaiaurcode" someObj ={newArr} />
-      <Card/>
+      <Card userName="Raja"btnText="Click Me" someObj ={newArr} />
+      <Card userName = "berlinbangkok" btnText = "Visit Me" />
     </>
   )
 }
