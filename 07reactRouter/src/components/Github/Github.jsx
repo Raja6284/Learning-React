81a3fc90c35ebe39d8e8b3e51cc334ad472f6087
Raja kumar <rajaraja360parth@gmail.com>
2024-07-05 18:36:50 +0530

    use of hook useLoaderData for api call

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github(){
 
-    const [data,setData] = useState([])
+        const data = useLoaderData()
+        
+    //const [data,setData] = useState([])
 
-    useEffect(()=>{
-        fetch('https://api.github.com/users/Raja6284')
-        .then(response=>response.json())
-        .then(data =>{
-            console.log(data)
-            setData(data)
-        })
-    },[])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/Raja6284')
+    //     .then(response=>response.json())
+    //     .then(data =>{
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // },[])
     return (
         <div className='bg-gray-600 p-4 m-4 text-3xl'>Github followers: {data.followers}
         <img src={data.avatar_url} alt="GitHub Picture" width={300}/>
@@ -21,3 +24,7 @@ function Github(){
 
 export default Github
 
+export const githubInfoLoader = async()=>{
+    const response = await fetch('https://api.github.com/users/Raja6284')
+    return response.json()
+}
