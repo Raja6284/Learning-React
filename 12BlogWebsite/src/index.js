8566e00c7022e5663a4eef07e2d5c3097a640496
Raja kumar <rajaraja360parth@gmail.com>
2024-09-09 12:33:36 +0530

    production grade react components

--- 12BlogWebsite/src/index.js
+++ 12BlogWebsite/src/index.js
@@ -1,7 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer/Footer";
-
+import Container from "./components/container/Container";
+import Logo from "./components/Logo";
+import LogoutBtn from "./components/Header/LogoutBtn";
 export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
