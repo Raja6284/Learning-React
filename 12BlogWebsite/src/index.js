0ba488a07bcd25ff29cad1479c8352a1ea05ba13
Raja kumar <rajaraja360parth@gmail.com>
2024-09-21 18:59:15 +0530

    adding form and slug values

--- 12BlogWebsite/src/index.js
+++ 12BlogWebsite/src/index.js
@@ -3,10 +3,12 @@ import Footer from "./components/Footer/Footer";
 import Container from "./components/container/Container";
 import Logo from "./components/Logo";
 import LogoutBtn from "./components/Header/LogoutBtn";
+import RTE from "./components/RTE";
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
